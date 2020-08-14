d7ba6e51fd1bc2afa1f134b6c9543d04a244bf93
zs <819201402@qq.com>
2020-08-14 15:33:04 +0800

    新添了一个分支

--- test2/src/test2/Helloworld2.java
+++ test2/src/test2/Helloworld2.java
@@ -1,7 +1,17 @@
 package test2;
 
+import java.time.LocalDateTime;
+import java.util.Calendar;
+
 public interface Helloworld2 {
 	public void fun();
 	
 	public void fun1();
+	
+	public static void main(String[] args) {
+	
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -1);
+		
+	}
 }
